de5317aa722de8b0782acdca0fc194d44f1452ea
Alina Orekhova <orehus.lina@mail.ru>
2024-06-24 00:37:47 +0400

    Added add and filter resits buttons

--- app/src/main/java/com/example/firstapp/FrontPageActivity.java
+++ app/src/main/java/com/example/firstapp/FrontPageActivity.java
@@ -3,6 +3,7 @@ package com.example.firstapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,15 +12,20 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.firstapp.fragments.AllResitsFragment;
+import com.example.firstapp.fragments.FilterResitsFragment;
 import com.example.firstapp.fragments.MyResitsFragment;
 import com.example.firstapp.fragments.ProfileFragment;
+import com.example.firstapp.fragments.AddResitFragment;
 import com.example.firstapp.model.User;
 import com.example.firstapp.model.UserViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FrontPageActivity extends AppCompatActivity {
     private UserViewModel userViewModel;
     private Toolbar toolbar;
+    private FloatingActionButton fabAddResit;
+    private FloatingActionButton fabFilterResit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,23 @@ public class FrontPageActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        fabAddResit = findViewById(R.id.fab_add_resit);
+        fabFilterResit = findViewById(R.id.fab_filter_resit);
+
+        fabAddResit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFragment(new AddResitFragment());
+            }
+        });
+
+        fabFilterResit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFragment(new FilterResitsFragment());
+            }
+        });
 
         // Получение данных о пользователе из Intent, если они есть
         Intent intent = getIntent();
@@ -73,14 +96,17 @@ public class FrontPageActivity extends AppCompatActivity {
                         case R.id.nav_all_resits:
                             selectedFragment = new AllResitsFragment();
                             toolbar.setTitle(R.string.all_resits_title);
+                            showFabButtons();
                             break;
                         case R.id.nav_my_resits:
                             selectedFragment = new MyResitsFragment();
                             toolbar.setTitle(R.string.my_resits_title);
+                            showFabButtons();
                             break;
                         case R.id.nav_profile:
                             selectedFragment = new ProfileFragment();
                             toolbar.setTitle(R.string.profile_title);
+                            hideFabButtons();
                             break;
                     }
 
@@ -90,4 +116,21 @@ public class FrontPageActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
+    private void openFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    private void showFabButtons() {
+        fabAddResit.show();
+        fabFilterResit.show();
+    }
+
+    private void hideFabButtons() {
+        fabAddResit.hide();
+        fabFilterResit.hide();
+    }
 }
